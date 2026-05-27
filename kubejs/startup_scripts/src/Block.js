16d3abc19a34549f0a7873637ed5c75b7023eff9
Suwako <1462579982@qq.com>
2026-05-28 00:21:29 +0800

    fix bug

--- kubejs/startup_scripts/src/Block.js
+++ kubejs/startup_scripts/src/Block.js
@@ -1,5 +1,5 @@
 StartupEvents.registry("block", e => {
-    e.create("andesite_original_ore_rock", "basic")
+    e.create("andesite_original_ore_rock") // 关键修改：删除了后面的 "basic"
     .soundType('amethyst')
     .unbreakable()
 })
