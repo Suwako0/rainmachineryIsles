357e8a54c8f71b688db52f5a1e5e720293f314a2
Suwako <1462579982@qq.com>
2026-05-28 19:05:10 +0800

    kjs update

--- kubejs/startup_scripts/src/Block.js
+++ kubejs/startup_scripts/src/Block.js
@@ -2,5 +2,5 @@ StartupEvents.registry("block", e => {
     e.create("andesite_original_ore_rock")
     .soundType('amethyst')
     .unbreakable()
-    .lightLevel(7 / 15.0)
+    .lightLevel(1.0)
 })
---
+++ kubejs/startup_scripts/src/Item.js
@@ -0,0 +1,4 @@
+StartupEvents.registry("item", e => {
+    //安山粉屑
+    e.create('andesite_dust')
+})
